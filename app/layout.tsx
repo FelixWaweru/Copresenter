b782ed8e84f77f5952f3093d75562b40f50be5bd
unknown <wawerufelix60@gmail.com>
2023-08-01 04:55:18 +0300

    feat: Vercel analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,14 @@
 import './globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
+import { Analytics } from '@vercel/analytics/react';
+import Head from 'next/head';
 
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
   title: 'Copresenter',
-  description: 'Your personal AI co presenter',
+  description: 'A virtual co-host that makes presentations a breeze by using AI to read out your slides, freeing you from prep hassles and letting you focus on delivery.',
 }
 
 export default function RootLayout({
@@ -16,7 +18,29 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+        <Head>
+          <title>Copresenter</title>
+          <meta property="og:title" content="Copresenter" key="title" />
+          <meta name="description" content="Copresenter: A virtual co-host that makes presentations a breeze by using AI to read out your slides, freeing you from prep hassles and letting you focus on delivery." />
+
+          <meta property="og:url" content="https://copresenter.vercel.app/" />
+          <meta property="og:type" content="website" />
+          <meta property="og:title" content="Copresenter: A virtual co-host that makes presentations a breeze by using AI to read out your slides, freeing you from prep hassles and letting you focus on delivery." />
+          <meta property="og:description" content="Copresenter: A virtual co-host that makes presentations a breeze by using AI to read out your slides, freeing you from prep hassles and letting you focus on delivery." />
+          <meta property="og:image" content="https://github.com/FelixWaweru/Copresenter/blob/6760952d485d7228b92590e55c4535ba299ce533/public/index.png" />
+
+          <meta name="twitter:card" content="summary_large_image" />
+          <meta property="twitter:domain" content="copresenter.vercel.app" />
+          <meta property="twitter:url" content="https://copresenter.vercel.app/" />
+          <meta name="twitter:title" content="Copresenter: A virtual co-host that makes presentations a breeze by using AI to read out your slides, freeing you from prep hassles and letting you focus on delivery." />
+          <meta name="twitter:description" content="Copresenter: A virtual co-host that makes presentations a breeze by using AI to read out your slides, freeing you from prep hassles and letting you focus on delivery." />
+          <meta name="twitter:image" content="https://github.com/FelixWaweru/Copresenter/blob/6760952d485d7228b92590e55c4535ba299ce533/public/index.png" />
+        </Head>
+
+      <body className={inter.className}>
+        {children}
+        <Analytics />
+      </body>
     </html>
   )
 }
