9a35254132fb7eea495be0657857d06aafdc6052
unknown <wawerufelix60@gmail.com>
2023-08-01 05:06:59 +0300

    feat: updated links

--- app/layout.tsx
+++ app/layout.tsx
@@ -27,14 +27,14 @@ export default function RootLayout({
           <meta property="og:type" content="website" />
           <meta property="og:title" content="Copresenter: A virtual co-host that makes presentations a breeze by using AI to read out your slides, freeing you from prep hassles and letting you focus on delivery." />
           <meta property="og:description" content="Copresenter: A virtual co-host that makes presentations a breeze by using AI to read out your slides, freeing you from prep hassles and letting you focus on delivery." />
-          <meta property="og:image" content="https://copresenter.vercel.app/index.png" />
+          <meta property="og:image" content="https://copresenter.vercel.app/index.webp" />
 
           <meta name="twitter:card" content="summary_large_image" />
           <meta property="twitter:domain" content="copresenter.vercel.app" />
           <meta property="twitter:url" content="https://copresenter.vercel.app/" />
           <meta name="twitter:title" content="Copresenter: A virtual co-host that makes presentations a breeze by using AI to read out your slides, freeing you from prep hassles and letting you focus on delivery." />
           <meta name="twitter:description" content="Copresenter: A virtual co-host that makes presentations a breeze by using AI to read out your slides, freeing you from prep hassles and letting you focus on delivery." />
-          <meta name="twitter:image" content="https://copresenter.vercel.app/index.png" />
+          <meta name="twitter:image" content="https://copresenter.vercel.app/index.webp" />
         </Head>
 
       <body className={inter.className}>
